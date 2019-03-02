b12527d7d8af881cf49d55fa771acff57f402c20
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-02 17:14:47 -0500

    Verbose verify command

--- svt-app-operator/script/operator_courier_verify.sh
+++ svt-app-operator/script/operator_courier_verify.sh
@@ -7,4 +7,6 @@ set -o pipefail
 readonly BUNDLE_DIR="${GOPATH}/src/github.com/hongkailiu/operators/svt-app-operator/deploy/BUNDLE_DIR"
 echo "ls -al ${BUNDLE_DIR}"
 ls -al "${BUNDLE_DIR}"
+echo "operator-courier verify ${BUNDLE_DIR} ..."
 operator-courier verify "${BUNDLE_DIR}"
+echo "operator-courier exited with $?"
