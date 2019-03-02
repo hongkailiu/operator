9562e4449da02cd7e3305627a5a400bec0f0c881
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-02 15:13:39 -0500

    Update install

--- script/ci/install_dep.sh
+++ script/ci/install_dep.sh
@@ -29,17 +29,13 @@ cd "${current_dir}" || exit 1
 echo "installing minikube"
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
   chmod +x minikube && sudo mv minikube /usr/local/bin/
-###https://github.com/kubernetes/minikube/issues/2176
-#export MINIKUBE_HOME=$HOME
 export CHANGE_MINIKUBE_NONE_USER=true
-#export KUBECONFIG=$HOME/.kube/config
 echo "starting minikube"
 sudo minikube start --vm-driver=none --kubernetes-version=v1.13.3
-echo "minikube update-context ..."
 ls -al "$HOME/.kube/config"
 cat "$HOME/.kube/config"
+echo "minikube update-context ..."
 sudo minikube update-context
-ls -al "$HOME/.minikube/"
 readonly UUU="$(id -u -n)"
 readonly GGG="$(id -u -n)"
 echo "UUU=${UUU}; GGG=${GGG}"
