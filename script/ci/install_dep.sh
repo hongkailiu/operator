495daa8f0a111eb70189b971ce0f915823bdaad8
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-02 11:53:47 -0500

    Fix kubectl

--- script/ci/install_dep.sh
+++ script/ci/install_dep.sh
@@ -2,10 +2,11 @@
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
 chmod +x ./kubectl
-sudo mv -v ./kubectl /usr/bin/
+sudo mv -v ./kubectl /usr/local/bin/
 
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
+current_dir="$(pwd)"
 mkdir -p ${GOPATH}/src/github.com/operator-framework
 cd ${GOPATH}/src/github.com/operator-framework
 git clone https://github.com/operator-framework/operator-sdk
@@ -13,3 +14,4 @@ cd operator-sdk
 git checkout master
 make dep
 make install
+cd "${current_dir}" || exit 1
