0a6ed01743849223ac57f4acdceef1ad7a4fdb18
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-02 11:42:50 -0500

    Install kubectl 3

--- script/ci/install_dep.sh
+++ script/ci/install_dep.sh
@@ -2,7 +2,7 @@
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
 chmod +x ./kubectl
-mv -v ./kubectl /usr/bin/
+sudo mv -v ./kubectl /usr/bin/
 
 mkdir -p ${GOPATH}/src/github.com/operator-framework
 cd ${GOPATH}/src/github.com/operator-framework
